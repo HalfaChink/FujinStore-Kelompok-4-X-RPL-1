5d17af7db3e5abe91a93008cf81b2ec777372459
Barakataklmaoxd <16568584-Barakataklmaoxd@users.noreply.replit.com>
2023-05-22 04:49:09 +0000

    Remake metode pembayaran

--- topup.js
+++ topup.js
@@ -1,4 +1,16 @@
-let price = (price) => {
-  let no = document.getElementById("hasil")
-  no.value += angka;
+let price = (crystal, price) => {
+  if(confirm(`Apakah Anda Yakin Ingin Membeli ${crystal}  Dengan Harga ${price}?`)==true){
+    if(saldo < price){
+      alert('Saldo Anda Tidak Mencukupi')
+    }else{
+      const id = prompt('Masukan UID Anda')
+      document.getElementById('ket').innerHTML = `Anda Telah Membeli ${crystal} Dengan Harga ${price} Dengan UID ${id}`
+  alert('berhasil')
+  saldo = saldo - price
+      document.getElementById('saldo').innerHTML = `Saldo Anda ${saldo}`
+
+    }
+  }
 }
+let saldo = 2500000
+document.getElementById('saldo').innerHTML = `Saldo Anda ${saldo}`
