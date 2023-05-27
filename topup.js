edc0e239d4d7f96c4d989ee8500336d0aa680288
HalfaChink <85602339+HalfaChink@users.noreply.github.com>
2023-05-27 20:30:08 +0000

    Tambahan ML Page

--- topup.js
+++ topup.js
@@ -1,16 +1,16 @@
-let price = (crystal, price) => {
-  if(confirm(`Apakah Anda Yakin Ingin Membeli ${crystal}  Dengan Harga ${price}?`)==true){
+let price = (topup, price) => {
+  if(confirm(`Apakah Anda Yakin Ingin Membeli ${topup}  Dengan Harga ${price}?`)==true){
     if(saldo < price){
-      alert('Saldo Anda Tidak Mencukupi')
+      alert('Saldo Anda Tidak Cukup')
     }else{
       const id = prompt('Masukan UID Anda')
-      document.getElementById('ket').innerHTML = `Anda Telah Membeli ${crystal} Dengan Harga ${price} Dengan UID ${id}`
-  alert('berhasil')
+      document.getElementById('ket').innerHTML = `Anda Telah Membeli ${topup} Dengan Harga ${price} Untuk UID ${id}`
+  alert('Berhasil')
   saldo = saldo - price
       document.getElementById('saldo').innerHTML = `Saldo Anda ${saldo}`
 
     }
   }
 }
-let saldo = 2500000
-document.getElementById('saldo').innerHTML = `Saldo Anda ${saldo}`
+let saldo = 100000
+document.getElementById('saldo').innerHTML = `Saldo Anda IDR ${saldo}`
